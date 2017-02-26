b0ff2317e15166e477f87c848f07e5a22c433d2a
Artem <kakawkaization@gmaikl.com>
2017-02-26 21:33:26 +0300

    edited parameters sed

--- bsiclon.cpp
+++ bsiclon.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
 	vm1 = argv[1];
 	vm2 = argv[2];
 
-	sprintf(com, "rsync -a --info=progress2 /usr/vm/%s /usr/vm/%s; mv /usr/vm/%s/%s.img /usr/vm/%s/%s.img; mv /usr/vm/%s/%s.sh /usr/vm/%s/%s.sh; sed 's/%s/%s/g' /usr/vm/%s/%s.sh >> /usr/vm/%s/%s.sh", vm1, vm2, vm2, vm1, vm2, vm2, vm2, vm1, vm2, vm2, vm1, vm2, vm2, vm2, vm2, vm2);  
+	sprintf(com, "rsync -a --info=progress2 /usr/vm/%s/* /usr/vm/%s; mv /usr/vm/%s/%s.img /usr/vm/%s/%s.img; mv /usr/vm/%s/%s.sh /usr/vm/%s/%s.sh; sed -i 's/%s/%s/g' /usr/vm/%s/%s.sh", vm1, vm2, vm2, vm1, vm2, vm2, vm2, vm1, vm2, vm2, vm1, vm2, vm2, vm2);  
 	system(com);
 
 	return 0;
