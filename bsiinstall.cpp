e291d5e89f50eb58fe1670b42dd745662aea2923
SimpleBSD <admin@simplebsd.ru>
2017-03-17 21:04:49 +0300

    added

--- bsiinstall.cpp
+++ bsiinstall.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void install() {
 	int os, cpu, mem, disk;
-	char iso[150], tap[100], name[100], com1[300], com2[300], folder[150], filename[150], ufilename[150], sh_file[150], ush_file[300];
+	char iso[150], tap[100], name[100], com1[300], com2[300], folder[150], filename[150], uifilename[150], ifile[100], ufilename[150], sh_file[150], ush_file[300];
 
 	cout << "OS:\n";
 	cout << "1. FreeBSD.\n";
@@ -79,10 +79,20 @@ void install() {
                 osconf.open(filename);
 		osconf << "(hd0) /usr/bsi/vm/" << name << "/" << name << ".img\n(cd0) " << iso << "\n";
 		osconf.close();
+		sprintf(uifilename, "/usr/bsi/vm/%s/%s-install.sh", name, name);
+		ofstream ui_file;
+		ui_file.open(uifilename);
+		ui_file << "#!/bin/sh\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r cd0 -m /usr/bsi/vm/" << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c " << cpu << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << " \\\n-s 3,ahci-cd," << iso << " \\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << "\nbhyvectl --destroy --vm=" << name << "\n";
+		ui_file.close();
+		chmod(uifilename, S_IRWXU|S_IXGRP|S_IXOTH);
+		sprintf(ifile, "/usr/bsi/vm/%s/./%s-install.sh", name, name);
+		system(ifile);
+		//remove(uifilename);
 		sprintf(ufilename, "/usr/bsi/vm/%s/%s.sh", name, name);
 		ofstream ush_file;
 		ush_file.open(ufilename);
-		ush_file << "#!/bin/sh\nwhile [ 1 ]; do\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r hd0,msdos1 -m /usr/bsi/ << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c << cpu << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << "\\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << "\nbhyve_exit=$?\nif [$bhyve_exit -ne 0]; then\nbreak\nfi\ndone\nbhyvecdtl --destroy --vm=" << name << "\n";
+		ush_file << "#!/bin/sh\nwhile [ 1 ]; do\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r hd0,msdos1 -m /usr/bsi/vm/" << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c " << cpu << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << " \\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << " \nbhyve_exit=$?\nif [$bhyve_exit -ne 0]; then\nbreak\nfi\ndone\nbhyvectl --destroy --vm=" << name << "\n";
 		ush_file.close();
+		chmod(ufilename, S_IRWXU|S_IXGRP|S_IXOTH);
 	}
 }
