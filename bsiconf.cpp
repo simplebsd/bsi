08c8232bbbffcba095585db971ccf0c1e609de91
SimpleBSD <admin@simplebsd.ru>
2017-03-26 10:00:07 +0000

    edited bsiconf

--- bsiconf.cpp
+++ bsiconf.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <stdio.h>
+#include <fstream>
 using namespace std;
 
 void bsiconf(char *vm) {
+	char path_img[150], com[250], os[50];
+
 	sprintf(path_img, "/usr/bsi/vm/%s/%s.img", vm, vm);
 	ifstream vm_img(path_img);
 
 	if(vm_img) {
 
-		system("/bin/cat /usr/bsi/vm/f2z/f2z.sh | /usr/bin/grep vmrun | /usr/bin/cut -c27-31");
+		sprintf(com, "/bin/cat /usr/bsi/vm/%s/%s.sh | /usr/bin/grep OS | /usr/bin/cut -c 6-15", vm, vm);
+		os = system(com);
 		
 		cout << "OS: " << os;
 
