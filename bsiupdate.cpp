784b32c1fe6c54e54a2c859a51ab125c6ef50fb9
SimpleBSD <admin@simplebsd.ru>
2017-06-26 00:12:53 +0300

    added function update

--- bsiupdate.cpp
+++ bsiupdate.cpp
@@ -1,3 +1,6 @@
 void update() {
-	system("/usr/local/bin/git clone https://github.com/simplebsd/bsi /tmp");
+	system("/usr/local/bin/git clone https://github.com/simplebsd/bsi /usr/bsi/update");
+	system("cd /usr/bsi/update && make update");
+	system("rm -rf /usr/bsi/update/*");
+	system("rm -rf /usr/bsi/update/.git");
 }
