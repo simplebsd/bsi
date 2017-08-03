f10ebfadb483cf7811cc3436abea9385c0d49705
SimpleBSD <admin@simplebsd.ru>
2017-08-04 00:03:48 +0300

    changed links for download iso

--- downloadiso.cpp
+++ downloadiso.cpp
@@ -8,8 +8,8 @@ void diso() {
 	cout << "3. Ubuntu 16.04 x64.\n";
 	cin >> i;
 
-	if(i == 1) system("fetch http://ftp.freebsd.org/pub/FreeBSD/releases/ISO-IMAGES/11.0/FreeBSD-11.0-RELEASE-i386-disc1.iso -o /usr/bsi/iso");
-	else if(i == 2) system("fetch http://ftp.freebsd.org/pub/FreeBSD/releases/ISO-IMAGES/11.0/FreeBSD-11.0-RELEASE-amd64-disc1.iso -o /usr/bsi/iso");
-	else if(i == 3) system("fetch http://releases.ubuntu.com/16.04/ubuntu-16.04.2-server-amd64.iso -o /usr/bsi/iso");
+	if(i == 1) system("fetch ftp://ftp.freebsd.org/pub/FreeBSD/releases/i386/i386/ISO-IMAGES/11.1/FreeBSD-11.1-RELEASE-i386-disc1.iso -o /usr/bsi/iso");
+	else if(i == 2) system("fetch ftp://ftp.freebsd.org/pub/FreeBSD/releases/amd64/amd64/ISO-IMAGES/11.1/FreeBSD-11.1-RELEASE-amd64-disc1.iso -o /usr/bsi/iso");
+	else if(i == 3) system("fetch http://releases.ubuntu.com/16.04/ubuntu-16.04.3-server-amd64.iso -o /usr/bsi/iso");
 	else cout << "Choose correct answer.\n";
 }
