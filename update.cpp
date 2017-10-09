00e25946de4af1a809f27d3ef5665de97b820f3a
SimpleBSD <admin@simplebsd.ru>
2017-10-09 22:34:30 +0300

    bug fixed

--- update.cpp
+++ update.cpp
@@ -1,5 +1,5 @@
 void update() {
-	system("/usr/local/bin/git clone https://github.com/simplebsd/bsi /usr/bsi/update");
+	system("/usr/local/bin/git clone https://github.com/simplebsd/bsi /usr/local/etc/bsi/update");
 	system("cd /usr/local/etc/bsi/update && make update");
 	system("rm -rf /usr/local/etc/bsi/update/*");
 	system("rm -rf /usr/local/etc/bsi/update/.git");
