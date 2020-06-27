99431dd1937b405e76e3546c267971186ae17dae
simplebsd <admin@simplebsd.ru>
2020-06-27 11:47:40 +0600

    add new iso

--- downloadiso.cpp
+++ downloadiso.cpp
@@ -8,8 +8,8 @@ void diso() {
 	cout << "3. Ubuntu 16.04 x64.\n";
 	cin >> i;
 
-	if(i == 1) system("fetch https://mirror.yandex.ru/freebsd/releases/ISO-IMAGES/11.1/FreeBSD-11.1-RELEASE-i386-disc1.iso -o /usr/bsi/iso");
-	else if(i == 2) system("fetch https://mirror.yandex.ru/freebsd/releases/ISO-IMAGES/11.1/FreeBSD-11.1-RELEASE-amd64-disc1.iso -o /usr/bsi/iso");
+	if(i == 1) system("fetch https://mirror.yandex.ru/freebsd/releases/ISO-IMAGES/12.1/FreeBSD-12.1-RELEASE-i386-disc1.iso -o /usr/bsi/iso");
+	else if(i == 2) system("fetch https://mirror.yandex.ru/freebsd/releases/ISO-IMAGES/12.1/FreeBSD-12.1-RELEASE-amd64-disc1.iso -o /usr/bsi/iso");
 	else if(i == 3) system("fetch http://releases.ubuntu.com/16.04/ubuntu-16.04.3-server-amd64.iso -o /usr/bsi/iso");
 	else cout << "Choose correct answer.\n";
 }
