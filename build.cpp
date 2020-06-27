d07ca5f3c6c1d8e89f235366ec42704532b4f7f2
simplebsd <admin@simplebsd.ru>
2020-06-27 14:32:44 +0600

    change cpu to cores var

--- build.cpp
+++ build.cpp
@@ -62,7 +62,7 @@ void build() {
     }
 }
 void install() {
-	int os, cpu, mem, disk, t, nt;
+	int os, cores, mem, disk, t, nt;
 	char iso[150], tap[100], name[100], com1[300], com2[300], folder[150], filename[150], uifilename[150], ifile[100], ufilename[150], sh_file[150], ush_file[300], br1[150], br2[150], atp[300];
 
 	cout << "OS:\n";
@@ -86,7 +86,7 @@ void install() {
 		cin >> iso;
 		cout << "\n";
 		cout << "Enter number of cores CPU(1-16): ";
-		cin >> cpu;
+		cin >> cores;
 		cout << "\n";
 		cout << "Enter size of memory(MB): ";
 		cin >> mem;
@@ -118,11 +118,11 @@ system(atp);
 		sprintf(filename, "/usr/bsi/vm/%s/%s.sh", name, name);
 		ofstream osconf;
 		osconf.open(filename);
-		osconf << "#!/bin/sh\n\n#OS: FreeBSD\n#CPU(cores): " << cpu << "\n#Memory(MB): " << mem << "\n#Disk(GB): " << disk << "\n#Network : " << tap << "\n\n/usr/share/examples/bhyve/vmrun.sh -c " << cpu << " -m " <<  mem << " -t " << tap << " -d " << name << ".img " << name << "\n";
+		osconf << "#!/bin/sh\n\n#OS: FreeBSD\n#CPU(cores): " << cores << "\n#Memory(MB): " << mem << "\n#Disk(GB): " << disk << "\n#Network : " << tap << "\n\n/usr/share/examples/bhyve/vmrun.sh -c " << cores << " -m " <<  mem << " -t " << tap << " -d " << name << ".img " << name << "\n";
 		osconf.close();
 		sprintf(sh_file, "/usr/bsi/vm/%s/%s.sh", name, name);
 		chmod(sh_file, S_IRWXU|S_IXGRP|S_IXOTH);
-		sprintf(com2, "/usr/share/examples/bhyve/vmrun.sh -c %i -m %i -t %s -d /usr/bsi/vm/%s/%s.img -i -I %s %s", cpu, mem, tap, name, name, iso, name);
+		sprintf(com2, "/usr/share/examples/bhyve/vmrun.sh -c %i -m %i -t %s -d /usr/bsi/vm/%s/%s.img -i -I %s %s", cores, mem, tap, name, name, iso, name);
 		system(com2);	
 	}
 	
@@ -139,7 +139,7 @@ system(atp);
                 cin >> iso;
                 cout << "\n";
                 cout << "Enter number of cores CPU(1-16): ";
-                cin >> cpu;
+                cin >> cores;
                 cout << "\n";
                 cout << "Enter size of memory(MB): ";
                 cin >> mem;
@@ -176,7 +176,7 @@ system(atp);
 		sprintf(uifilename, "/usr/bsi/vm/%s/%s-install.sh", name, name);
 		ofstream ui_file;
 		ui_file.open(uifilename);
-		ui_file << "#!/bin/sh\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r cd0 -m /usr/bsi/vm/" << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c " << cpu << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << " \\\n-s 3,ahci-cd," << iso << " \\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << "\nbhyvectl --destroy --vm=" << name << "\n";
+		ui_file << "#!/bin/sh\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r cd0 -m /usr/bsi/vm/" << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c " << cores << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << " \\\n-s 3,ahci-cd," << iso << " \\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << "\nbhyvectl --destroy --vm=" << name << "\n";
 		ui_file.close();
 		chmod(uifilename, S_IRWXU|S_IXGRP|S_IXOTH);
 		sprintf(ifile, "/usr/bsi/vm/%s/./%s-install.sh", name, name);
@@ -185,7 +185,7 @@ system(atp);
 		sprintf(ufilename, "/usr/bsi/vm/%s/%s.sh", name, name);
 		ofstream ush_file;
 		ush_file.open(ufilename);
-		ush_file << "#!/bin/sh\n\n#OS: Ubuntu\n#CPU(cores): " << cpu << "\n#Memory(MB): " << mem << "\n#Disk(GB): " << disk << "\n#Network : " << tap << "\n\n" << "#!/bin/sh\nwhile [ 1 ]; do\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r hd0,msdos1 -m /usr/bsi/vm/" << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c " << cpu << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << " \\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << " \nbhyve_exit=$?\nif [ $bhyve_exit -ne 0 ]; then\nbreak\nfi\ndone\nbhyvectl --destroy --vm=" << name << "\n";
+		ush_file << "#!/bin/sh\n\n#OS: Ubuntu\n#CPU(cores): " << cores << "\n#Memory(MB): " << mem << "\n#Disk(GB): " << disk << "\n#Network : " << tap << "\n\n" << "#!/bin/sh\nwhile [ 1 ]; do\nbhyvectl --destroy --vm=" << name << "\ngrub-bhyve -r hd0,msdos1 -m /usr/bsi/vm/" << name << "/device.map -M " << mem << " " << name << "\n\nbhyve -c " << cores << " -m " << mem << " -H -P -A \\\n-l com1,stdio \\\n-s 0:0,hostbridge \\\n-s 1:0,lpc \\\n-s 2:0,virtio-net," << tap << " \\\n-s 4,virtio-blk,/usr/bsi/vm/" << name << "/" << name << ".img " << name << " \nbhyve_exit=$?\nif [ $bhyve_exit -ne 0 ]; then\nbreak\nfi\ndone\nbhyvectl --destroy --vm=" << name << "\n";
 		ush_file.close();
 		chmod(ufilename, S_IRWXU|S_IXGRP|S_IXOTH);
 
