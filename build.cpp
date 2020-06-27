5ff0f6656f1ef919f130c9a4cf770cfc0669f520
simplebsd <admin@simplebsd.ru>
2020-06-27 14:41:26 +0600

    del char iso

--- build.cpp
+++ build.cpp
@@ -63,7 +63,7 @@ void build() {
 }
 void install() {
 	int t, nt;
-	char iso[150], tap[100], name[100], com1[300], com2[300], folder[150], filename[150], uifilename[150], ifile[100], ufilename[150], sh_file[150], ush_file[300], br1[150], br2[150], atp[300];
+	char tap[100], name[100], com1[300], com2[300], folder[150], filename[150], uifilename[150], ifile[100], ufilename[150], sh_file[150], ush_file[300], br1[150], br2[150], atp[300];
 
 	cout << "OS:\n";
 	cout << "1. FreeBSD.\n";
